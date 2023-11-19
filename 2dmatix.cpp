0940827496c53d0d4aba4949e8e123d9af0d9d9f
lotading <aksel.joakim06@gmail.com>
2023-11-19 04:45:03 +0100

    la til en switch statement

--- 2dmatix.cpp
+++ 2dmatix.cpp
@@ -6,17 +6,20 @@ class Solution {
 public:
     bool searchMatrix(std::vector<std::vector<int>>& matrix, int target) {
 
-        int m,n;
-
         for (int i = 0; i < matrix.size(); i++) {
 
             for (int j = 0; j < matrix[i].size(); j++) {
 
-                if (matrix[i][j] == target) {
-                return true;
+                int matrixed = matrix[i][j];
+
+                switch (matrix[i][j] == target) {
+                    
+                case true:
+                    return true;
+                default:
+                    break;
                 }
             }
-            
         }
         return false;
     }
